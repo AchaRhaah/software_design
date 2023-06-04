8a3641651278b9289385f34f0e0874b447eb366c
xennia316 <xennia316@gmail.com>
2023-06-04 18:56:08 +0100

    view route page mount

--- src/pages/ViewRoutes/ViewRoutes.page.jsx
+++ src/pages/ViewRoutes/ViewRoutes.page.jsx
@@ -1,47 +1,16 @@
 import React from "react";
 import styles from "./viewRoutes.module.css";
-import { LocationField, Tabs, Footer, TopCard } from "../../components";
-import arrowBack from "../../assets/bi_arrow-left.svg";
+import { LocationField } from "../../components";
 
 const ViewRoutes = () => {
-	const texts = [
-		{ top: "Bla bla bla", bottom: "Buea" },
-		{ top: "Cite La plaza, Molyko", bottom: "Limbe" },
-		{ top: "Bla bla bla", bottom: "Tiko" },
-		{ top: "Bla bla bla", bottom: "Kumba" },
-		{ top: "Bla bla bla", bottom: "Douala" },
-	];
-
-	const header = {text: "Blanco Disposal Services", buttonText:"Subscribe", buttonDisabled:false}
 	return (
 		<section className={`${styles.pageWrapper}`}>
-			<img className={`${styles.backArrow}`} src={arrowBack} alt="" />
-			<TopCard text={header.text} buttonText={header.buttonText} buttonDisabled={header.buttonDisabled}/>
-			<div className={`${styles.tabContainer}`}>
-				<Tabs
-					tabs={[
-						"Monday",
-						"Tuesday",
-						"Wednesday",
-						"Thursday",
-						"Friday",
-						"Saturday",
-						"Sunday",
-					]}
-					activeTab={2}
-					setActiveTab={""}
-				/>
-				{texts.map((item, index) => {
-					return (
-						<LocationField
-							topText={item.top}
-							bottomText={item.bottom}
-							key={index}
-						/>
-					);
-				})}
-			</div>
-			<Footer />
+			<section className={`${styles.topCard}`}>
+				<p className={`${styles.text}`}>
+					Blanco Disposal <br /> Services
+				</p>
+			</section>
+			<LocationField />
 		</section>
 	);
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 export { default as Welcome } from "./Welcome/Welcome";
 export { default as LetsGo } from "./Letsgo/LetsGo";
 export { default as CreateAcc } from "./createAcc/CreateAcc";
-export { default as ViewRoutes } from "./ViewRoutes/ViewRoutes.page.jsx";
 export { default as PublishRoutes } from "./publishRoutes/PublishRoutes";
-export { default as CustomerHome } from "./customerHome/CustomerHome";
+export { default as ViewRoutes } from "./ViewRoutes/ViewRoutes.page.jsx";
