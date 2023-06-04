493f340dbd093d2ce267c00003b277195a3c273d
xennia316 <xennia316@gmail.com>
2023-06-04 18:55:42 +0100

    add route to page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { Welcome, LetsGo, CreateAcc } from "./pages";
+import { Welcome, LetsGo, CreateAcc, ViewRoutes } from "./pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" exact element={<Welcome />} />
-          <Route path="/letsGo" exact element={<LetsGo />} />
-          <Route path="/create-account" exact element={<CreateAcc />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" exact element={<Welcome />} />
+					<Route path="/letsGo" exact element={<LetsGo />} />
+					<Route path="/create-account" exact element={<CreateAcc />} />
+					<Route path="/view-routes" exact element={<ViewRoutes />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
