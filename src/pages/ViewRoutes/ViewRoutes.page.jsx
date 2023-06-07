7bf942b659570855a28530a59fb4ec8f3ce39482
xennia316 <xennia316@gmail.com>
2023-06-07 19:54:10 +0100

    setup: page mount

--- src/pages/ViewRoutes/ViewRoutes.page.jsx
+++ src/pages/ViewRoutes/ViewRoutes.page.jsx
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./viewRoutes.module.css";
-import { LocationField } from "../../components";
+import { LocationField, PrimaryButton, Tabs } from "../../components";
+import arrowBack from "../../assets/bi_arrow-left.svg";
 
 const ViewRoutes = () => {
+	const texts = [
+		{ top: "Bla bla bla", bottom: "Buea" },
+		{ top: "Cite La plaza, Molyko", bottom: "Limbe" },
+		{ top: "Bla bla bla", bottom: "Tiko" },
+		{ top: "Bla bla bla", bottom: "Kumba" },
+		{ top: "Bla bla bla", bottom: "Douala" },
+	];
+
+	const [isActive, setIsActive] = useState(false);
 	return (
 		<section className={`${styles.pageWrapper}`}>
-			<section className={`${styles.topCard}`}>
+			<img className={`${styles.backArrow}`} src={arrowBack} />
+			<div className={`${styles.topCard}`}>
 				<p className={`${styles.text}`}>
 					Blanco Disposal <br /> Services
 				</p>
-			</section>
-			<LocationField />
+				<div className={`${styles.btnWrapper}`}>
+					<PrimaryButton text={"Subscribe"} disabled={false} />
+				</div>
+			</div>
+			<div className={`${styles.tabContainer}`}>
+				<Tabs
+					tabs={[
+						"Monday",
+						"Tuesday",
+						"Wednesday",
+						"Thursday",
+						"Friday",
+						"Saturday",
+						"Sunday",
+					]}
+					activeTab={2}
+				/>
+				{texts.map((item, index) => {
+					return (
+						<LocationField
+							topText={item.top}
+							bottomText={item.bottom}
+							key={index}
+						/>
+					);
+				})}
+			</div>
 		</section>
 	);
 };
