7bbd6e84b6528603f89c8530bb69fde360322791
xennia316 <xennia316@gmail.com>
2023-06-07 19:53:00 +0100

    export tab component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as GreenBtn } from "./greenBtn/GreenBtn";
 export { default as PhoneInput } from "./phoneInput/PhoneInput";
 export { default as PasswordInput } from "./passwordInput/PasswordInput";
 export { default as LocationField } from "./locationField/locationField.component";
+export { default as PrimaryButton } from "./primaryBtn/PrimaryButton";
+export { default as Tabs } from "./tabs/Tabs";
