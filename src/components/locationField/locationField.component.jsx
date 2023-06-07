ecefa61691d154703eb83b6d6b423cd44c7e3fe1
xennia316 <xennia316@gmail.com>
2023-06-07 19:53:12 +0100

    update: props

--- src/components/locationField/locationField.component.jsx
+++ src/components/locationField/locationField.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./locationField.module.css";
 
-const LocationField = () => {
+const LocationField = ({ topText, bottomText }) => {
 	return (
 		<div className={`${styles.wrapper}`}>
-			<p className={`${styles.topText}`}>Cite La paza, Malingo</p>
-			<p className={`${styles.bottomText}`}>Buea</p>
+			<p className={`${styles.topText}`}>{topText}</p>
+			<p className={`${styles.bottomText}`}>{bottomText}</p>
 		</div>
 	);
 };
