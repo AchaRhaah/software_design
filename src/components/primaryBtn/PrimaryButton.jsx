93f13a8a1a5b751af1d99730912f00b401050593
Blanco237 <asongrandy9@gmail.com>
2023-06-09 18:33:42 +0100

    Primary Btn and Tabs Component

---
+++ src/components/primaryBtn/PrimaryButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './btn.module.css'
+
+const PrimaryButton = ({ onClick, text, disabled}) => {
+  return (
+    <button onClick={onClick} className={styles.btnContainer} disabled>
+      {text}
+    </button>
+  )
+}
+
+export default PrimaryButton
