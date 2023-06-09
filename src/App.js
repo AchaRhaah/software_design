9ebd93cd70e752cb9cdc93b74b4ace7c84f43ac6
Blanco237 <asongrandy9@gmail.com>
2023-06-09 19:52:39 +0100

    Loader Component

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
 					<Route path="/" exact element={<Welcome />} />
 					<Route path="/letsGo" exact element={<LetsGo />} />
 					<Route path="/create-account" exact element={<CreateAcc />} />
-					<Route path="/view-routes" exact element={<ViewRoutes />} />
 				</Routes>
 			</BrowserRouter>
+			<Loader />
 		</div>
 	);
 }
