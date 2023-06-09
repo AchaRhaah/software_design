f7a0bb428a494d3cf98f3a9e07986562ef49ba4b
Blanco237 <asongrandy9@gmail.com>
2023-06-09 19:52:39 +0100

    Loader Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Welcome, LetsGo, CreateAcc } from "./pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Loader from "./components/loader/Loader";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/create-account" exact element={<CreateAcc />} />
         </Routes>
       </BrowserRouter>
+      <Loader />
     </div>
   );
 }
